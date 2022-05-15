f27b1b5b80470513463bfc07b6ba402e2b731e30
yslron <yslron1000@gmail.com>
2022-05-15 18:04:27 +0800

    "updated"

--- script.js
+++ script.js
@@ -13,44 +13,55 @@ r.innerHTML = "R: 0"
 g.innerHTML = "G: 0"
 b.innerHTML = "B: 0"
 
-c.innerHTML = "Color Lightness: "
+c.innerHTML = "Color Lightness Detected: "
 
-  
+
+const nodes = 4;
+
 const trainingData = [
-  { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { Light: 1 } },
-  { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { Light: 1 } },
-  { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { Dark: 1 } },
-  { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { Light: 1 } },
-  { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { Dark: 1 } },
-  { input: {r: 1, g: 0.99, b: 0}, output: { Light: 1 } },
-  { input: {r: 1, g: 0.42, b: 0.52}, output: { Dark: 1 } },
-  { input: {r: 0.03, g: 0.58, b: 0}, output: { Dark: 1 } },
-  { input: {r: 1, g: 0.36, b: 0.36}, output: { Light: 1 } },
-  { input: {r: 1, g: 0.0, b: 0.0}, output: { Dark: 1} },
-  { input: {r: 1, g: 0.6, b: 0.6}, output: { Light: 1} },
-  { input: {r: 0, g: 0.52, b: 0.5}, output: { Dark: 1} },
-  { input: {r: 0.93, g: 0.53, b: 1}, output: { Light: 1} },
-  { input: {r: 0.41, g: 0.6, b: 0}, output: { Dark: 1} }, //training data to train our AI
+
+  /**
+   * RBG coverted formula rgb = (rgb/255), rgb range (0-255)
+   * example: White Color is R:255 G:255 B:255 in RBG Value so we'll convert the RBG Value into Decimal for it to fit to the training data.
+   * Formula: where r,g,b = 255
+   * r/255 = 1, g/255 = 1, b/255 = 1 then rounded to the nearest 1
+   * 
+  **/
+  /**Blue Green**/ { input: { r: 1, g: 0.34, b: 0.2 }, output: { Light: 1 } },
+  /**Green**/ { input: { r: 0, g: 1, b: 0 }, output: { Light: 1 } },
+  /**Yellow**/ { input: { r: 1, g: 1, b: 0 }, output: { Light: 1 } },
+  /**Orange**/ { input: { r: 1, g: 0.65, b: 0 }, output: { Light: 1 } },
+  /**white**/ { input: { r: 1, g: 1, b: 1 }, output: { Light: 1 } },
+
+  /**Red**/ { input: { r: 1, g: 0, b: 0 }, output: { Dark: 1 } },
+  /**Purple**/ { input: { r: 0.5, g: 0, b: 0.5 }, output: { Dark: 1 } },
+  /**Violet**/ { input: { r: 0.5, g: 0, b: 1 }, output: { Dark: 1 } },
+  /**Blue**/ { input: { r: 0, g: 0, b: 1 }, output: { Dark: 1 } },
+  /**Black**/ { input: { r: 0, g: 0, b: 0 }, output: { Dark: 1 } },
+
+  /**Adjusted Input Own Data**/ 
+  { input: { r: 0, g: 0.5, b: 0.5 }, output: { Dark: 1 } },
+  { input: { r: 0.5, g: 0.39, b: 0 }, output: { Dark: 1 } },
+  { input: { r: 0.48, g: 0.54, b: 0 }, output: { Dark: 1 } },
+
 ];
+
+
+
 
 const config = {
   activation: 'sigmoid',
   inputSize: 5,
   inputRange: 5,
-  hiddenLayers: [5], //[nodes]
+  hiddenLayers: [nodes,nodes], //[nodes]
   outputSize: 5,
   learningRate: 0.1,
   decayRate: 0.999,
 };
 
 
-
-
-
-
 input.addEventListener("change", (e) => {
   const rgb = getRgb(e.target.value)
-
   const network = new brain.NeuralNetwork(config)
 
 
@@ -74,7 +85,7 @@ input.addEventListener("change", (e) => {
   r.innerHTML = "R: " + rgb.r
   g.innerHTML = "G: " + rgb.g
   b.innerHTML = "B: " + rgb.b
-  c.innerHTML = "Color Lightness: <b>" + result
+  c.innerHTML = "Color Lightness Detected: <b>" + result
   err.innerHTML = lol['error'].toFixed(5)
   ite.innerHTML = lol['iterations']
   console.log(rgb);
@@ -94,4 +105,5 @@ hid.innerHTML = hiddenLayers.length
 lea.innerHTML = learningRate
 inp.innerHTML = inputSize
 out.innerHTML = outputSize
+nod.innerHTML = nodes
 
